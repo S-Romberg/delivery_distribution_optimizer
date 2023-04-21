a668c333537d1d386d71e09d4ca6c022a4556e00
Spencer Romberg <spencer.romberg@gmail.com>
2023-04-20 21:52:16 -0600

    added id to location class, added distances to init

--- location.py
+++ location.py
@@ -1,10 +1,13 @@
 class Location:
-    def __init__(self, name, address):
+    def __init__(self, id, name, address):
+        print('Creating Location', id, name, address)
+        self.id = id
         self.name = name
         self.address = address
+        self.distances = None
 
     def set_distance(self, distance):
         self.distances = distance
 
     def __str__(self):
-        return self.name, self.address, self.distances
+        return self.id, self.name, self.address, self.distances
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from location import Location
 from hash_map import HashMap
 
 class Main:
-    locations = HashMap()
+    locations = HashMap(27)
     packages = HashMap(40)
     with open('data/Packages.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -13,12 +13,8 @@ class Main:
     with open('data/Addresses and Hubs.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for index, row in enumerate(reader):
-            locations.insert(Location(*row))
+            locations.insert(Location(index + 1, *row))
     with open('data/Distance.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         # for index, row in enumerate(reader):
             # locations[index].set_distance(row)
-
-    # print(HashMap())
-    # print(locations)
-    # print(packages)
